79e7d99b27b3c7889b7fc1a3f3139befefff3113
Magnus_Magnusson <magnusm15@ru.is>
2022-03-27 02:46:42 +0000

    Base front end react model. Person model

--- kattvardur/People/views.py
+++ kattvardur/People/views.py
@@ -2,8 +2,13 @@ from People.models import Person
 from People.serializers import PersonSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
+import django_filters.rest_framework
+
+from rest_framework import filters
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all().order_by('name')
     serializer_class = PersonSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['ssn']
