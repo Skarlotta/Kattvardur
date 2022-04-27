bbd96d903a4ce2636ae6b8815c5f20dc819d85b4
Magnús Á. Magnússon <magnusm15@ru.is>
2022-04-27 20:07:41 +0000

    Missed commit file

--- frontend/lib/components/search/searchbar.tsx
+++ frontend/lib/components/search/searchbar.tsx
@@ -2,13 +2,11 @@ import React, { useState} from 'react';
 import useSWR from 'swr'
 import styles from '../../styles/Search.module.css';
 
-// Declaring type of props - see "Typing Component Props" for more examples
 type Props = {
     onSearch : Function
-  }; /* use `interface` if exporting so that consumers can extend */
+  }; 
   
-  // Easiest way to declare a Function Component; return type is inferred.
-const Searchbar = ({onSearch }: Props) => {
+const Searchbar = ({onSearch}: Props) => {
     let [searchterm, changesearchterm] = useState("");
     return <form onSubmit={(e)=>{e.preventDefault()}}>
         <div className={styles.searchbar}>
