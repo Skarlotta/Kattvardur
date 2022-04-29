73f2a5138514b6b58d65233ad900fdfd183c5b4a
Magnús Ágúst <magnus@mmagnusson.net>
2022-04-29 23:09:18 +0000

    Cattery search

---
+++ frontend/lib/components/search/catterySearch/catterySearch.tsx
@@ -0,0 +1,24 @@
+import fetcher from '../../../../fetcher';
+import {GenericSearch} from '../genericSearch/genericSearch';
+import {GenericSearchResult} from '../genericSearch/searchresults';
+import {Cattery} from '../../../types';
+
+
+const styleData = (cattery:Cattery) => {
+    return <GenericSearchResult
+        url={'/raektun/'+cattery.id+"/"}
+        key={cattery.id}
+        heading={cattery.name}
+        subheading={cattery.country || ""}
+        topString={(cattery.address||"Ekkert heimilisfang") + ", " + (cattery.city||"óþekkt borg") + ", " + (cattery.country||"Óþekkt land")}
+        bottomString={(cattery.email||"Ekkert tölvupóstfang") + " - " + (cattery.phone||"Ekkert símanúmer") + " - " + (cattery.website||"Ekkert vefsvæði")}
+    />
+}
+
+export const CatterySearch = () => {
+    return <GenericSearch
+        title="Ræktunaleit"
+        url="/api/v1/cattery/"
+        styleData={styleData}
+    />
+}
--- frontend/pages/raektun/index.tsx
+++ frontend/pages/raektun/index.tsx
@@ -1,24 +1,8 @@
 import type { NextPage } from 'next'
-import { useState } from 'react';
-//import Searchpage from '../../lib/components/search/genericSearch/genericSearch';
-//import {CatterySearchResult} from '../../lib/components/search/genericSearch/searchresults';
-import Cattery from '../../models/Cattery';
-
-const processCatteryResults = (catteries : any[]) => {
-    let p = new Promise<Cattery[]>((resolve, fail) => {
-        let d:number = catteries.length;
-        let catteryArray : Cattery[] = [];
-        for(let cattery of catteries){
-            let newCattery = new Cattery(cattery);
-            catteryArray.push(newCattery);
-        }
-        resolve(catteryArray);
-    });
-    return p;
-};
+import {CatterySearch} from '../../lib/components/search/catterySearch/catterySearch'
 
 const CatterySearchPage: NextPage = () => {
-    return null;// <Searchpage title="Finna Ræktun" url="/api/v1/cattery/" styleData = {CatterySearchResult} processData={processCatteryResults}></Searchpage>;
+    return <CatterySearch/>;
 };
 
 export default CatterySearchPage
