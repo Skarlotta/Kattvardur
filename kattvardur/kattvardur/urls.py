7697405028ecab805d21f01deece66d99c04024d
Magnús Magnússon <magnus@mmagnusson.net>
2023-03-11 19:08:19 +0000

    static

--- kattvardur/kattvardur/urls.py
+++ kattvardur/kattvardur/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'api/v1/', include('API.urls', namespace='v1')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
